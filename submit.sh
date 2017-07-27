ad6b736a525e0b6dda6bfb993b964c0fa20e7bff
ano <anoidgit@users.noreply.github.com>
2017-07-27 08:04:16 +0800

    Update submit.sh

--- submit.sh
+++ submit.sh
@@ -14,6 +14,6 @@ python $toold/getzag.py $tmpd/cloze.valid.fq map.txt $tmpd/cloze.valid.targ
 python $toold/map.py $tmpd/cloze.valid.fq $tmpd/cloze.valid.map map.txt
 python $toold/jdata.py $tmpd/cloze.valid.map $tmpd/cloze.valid.targ $tmpd/valid.data
 
-th predict.lua
+th predict.lua $tmpd/valid.data $tmpd/aoanscore.txt
 
 python tools/nsel.py map.txt $tmpd/cloze.valid.fq $tmpd/aoanscore.txt $rsf
