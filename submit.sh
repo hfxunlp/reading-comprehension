4c25966f668c53232bef5c186e04f10ee87f3ec8
ano <anoidgit@users.noreply.github.com>
2017-07-27 08:10:44 +0800

    Update submit.sh

--- submit.sh
+++ submit.sh
@@ -6,8 +6,10 @@ export rsf=$2
 export tmpd="temp"
 export toold="tools"
 
-rm -fr $tmpd
-mkdir $tmpd
+if [ -d $tmpd]; then  
+  rm -fr $tmpd  
+fi
+mkdir -p $tmpd
 
 python $toold/filq4valid.py $srcf $tmpd/cloze.valid.fq
 python $toold/getzag.py $tmpd/cloze.valid.fq map.txt $tmpd/cloze.valid.targ
@@ -17,3 +19,5 @@ python $toold/jdata.py $tmpd/cloze.valid.map $tmpd/cloze.valid.targ $tmpd/valid.
 th predict.lua $tmpd/valid.data $tmpd/aoanscore.txt
 
 python tools/nsel.py map.txt $tmpd/cloze.valid.fq $tmpd/aoanscore.txt $rsf
+
+rm -fr $tmpd
